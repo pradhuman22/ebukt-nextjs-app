9f9d8c8cf72d76c71dcaf30fdb8af93a0903c9ae
pradhuman22 <shresthapradhuman2020@gmail.com>
2026-07-12 21:05:55 +0900

    Update .gitignore to include local.env and generated Prisma files. Add Prisma and related dependencies to package.json, including dotenv and pg. Simplify homepage component in page.tsx by removing unnecessary elements.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,3 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-
 export default function Home() {
-  return (
-    <div>
-      <h2 className="text-accent-foreground">Homepage</h2>
-      <Button className={"font-bold"}>
-        <Link href={"/"}> Go Back</Link>
-      </Button>
-    </div>
-  );
+  return <div>Homepage</div>;
 }
